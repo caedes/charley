e89205821d71b4dd31c277d7901bc048cc71d743
caedes <romain.laurent@wopata.com>
2014-06-30 12:27:55 +0200

    Burp

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-charley
-=======
+# Charley
 
 Charley is...
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>...::: Charley  :::...</title>
+  </head>
+
+  <body>
+    <?php echo $_GET['is']; ?>
+  </body>
+</html>
