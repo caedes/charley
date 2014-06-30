b17a295115003760966cc660b7f0d9a9c1599960
caedes <romain.laurent@wopata.com>
2014-06-30 14:00:58 +0200

    Burp

--- index.php
+++ index.php
@@ -2,31 +2,7 @@
   <head>
     <title>...::: Charley  :::...</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <style>
-    body {
-      background-image: url(images/burp.jpg);
-    }
-    body, a {
-      color: white;
-      font-family: "Comic Sans MS", "Comic Sans", cursive;
-    }
-    a {
-      text-decoration: none;
-    }
-    h1 {
-      font-weight: none;
-      font-size: 80px;
-      text-align: center;
-      margin-top: 300px;
-    }
-    footer {
-      text-align: center;
-      position: absolute;
-      bottom: 30px;
-      left: 50%;
-      margin-left: -90px;
-    }
-    </style>
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
   </head>
 
   <body>
